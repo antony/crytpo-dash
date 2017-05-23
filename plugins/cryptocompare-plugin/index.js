61575e1b7079efeaefcaf0d01e9310f4818b76ae
Antony Jones <aj@desirableobjects.co.uk>
2017-05-23 15:00:54 +0100

    Add coins, rejig

--- plugins/cryptocompare-plugin/index.js
+++ plugins/cryptocompare-plugin/index.js
@@ -8,32 +8,43 @@ const { Promise } = require('bluebird')
 class CryptocompareDatasource {
 
   constructor (options) {
-    this.symbol = options.symbol
+    this.symbols = options.symbols
   }
 
   getTimestamp (daysAgo) {
     return moment().subtract(daysAgo, 'days').unix()
   }
 
-  getData (ts) {
-    return got(
-      `https://min-api.cryptocompare.com/data/pricehistorical?fsym=${this.symbol}&tsyms=GBP&ts=${ts}`,
-      { json: true }
-    )
+  getUrlsForSymbol (symbol) {
+    return Promise.map([7,6,5,4,3,2,1], daysAgo => {
+      const timestamp = this.getTimestamp(daysAgo)
+      return this.getPriceUrl(symbol, timestamp)
+    })
+  }
+
+  getData (symbol) {
+    const reqs = this.getUrlsForSymbol(symbol)
+
+    return Promise.map(reqs, url => {
+      return got(url, { json: true })
+      .then(({ body }) => {
+        return reach(body, `${symbol}.GBP`)
+      })
+    })
+  }
+
+  getPriceUrl (symbol, ts) {
+    return `https://min-api.cryptocompare.com/data/pricehistorical?fsym=${symbol}&tsyms=GBP&ts=${ts}`
   }
 
   fetch () {
-    return Promise.map([7,6,5,4,3,2,1], daysAgo => {
-      const timestamp = this.getTimestamp(daysAgo)
-      
-      return this.getData(timestamp)
-      .then(({ body }) => {
-        return reach(body, `${this.symbol}.GBP`)
-      })
-    })
-    .then(data => {
-      return Promise.resolve({ [this.symbol]: data })
-    })
+    return Promise.reduce(this.symbols, (curr, symbol) => {
+      return this.getData(symbol)
+        .then((history) => {
+          curr[symbol] = history
+          return curr
+        })
+    }, {})
   }
 }
 
